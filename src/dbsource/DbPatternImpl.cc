ddbc7bd08856d4cf09c128327c3ba465e9be25c7
Klaus Kaempf <kkaempf@suse.de>
2006-03-16 00:41:51 +0000

    backup checkin (incomplete)

---
+++ src/dbsource/DbPatternImpl.cc
@@ -0,0 +1,105 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file zmd/backend/dbsource/DbPatternImpl.h
+ *
+*/
+
+#include "DbPatternImpl.h"
+#include "zypp/source/SourceImpl.h"
+#include "zypp/TranslatedText.h"
+#include "zypp/base/String.h"
+#include "zypp/base/Logger.h"
+
+using namespace std;
+using namespace zypp::detail;
+
+///////////////////////////////////////////////////////////////////
+namespace zypp
+{ /////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+//
+//        CLASS NAME : DbPatternImpl
+//
+///////////////////////////////////////////////////////////////////
+
+/** Default ctor
+*/
+DbPatternImpl::DbPatternImpl (Source_Ref source_r)
+    : _source (source_r)
+    , _zmdid(0)
+    , _default(false)
+    , _visible(false)
+{
+}
+
+/**
+ * read package specific data from handle
+ * throw() on error
+ */
+
+void
+DbPatternImpl::readHandle( sqlite_int64 id, sqlite3_stmt *handle )
+{
+    _zmdid = id;
+
+    // 1-5: nvra, see DbSourceImpl
+    _size_installed = sqlite3_column_int( handle, 6 );
+    // 7: catalog
+    // 8: installed
+    // 9: local
+    const char * text;
+    text = (const char *) sqlite3_column_text( handle, 12 );
+    if (text != NULL)
+	_summary = TranslatedText( string( text ) );
+    text = (const char *) sqlite3_column_text( handle, 13 );
+    if (text != NULL)
+	_description = TranslatedText( string( text ) );
+
+    return;
+}
+
+
+Source_Ref
+DbPatternImpl::source() const
+{ return _source; }
+
+/** Pattern summary */
+TranslatedText DbPatternImpl::summary() const
+{ return _summary; }
+
+/** Pattern description */
+TranslatedText DbPatternImpl::description() const
+{ return _description; }
+
+bool DbPatternImpl::isDefault() const
+{ return _default; }
+
+bool DbPatternImpl::userVisible() const
+{ return _visible; }
+
+TranslatedText DbPatternImpl::category() const
+{ return _category; }
+
+Pathname DbPatternImpl::icon() const
+{ return _icon; }
+
+Pathname DbPatternImpl::script() const
+{ return _script; }
+      
+Label DbPatternImpl::order() const
+{ return _order; }
+
+ZmdId DbPatternImpl::zmdid() const
+{ return _zmdid; }
+
+
+  /////////////////////////////////////////////////////////////////
+} // namespace zypp
+///////////////////////////////////////////////////////////////////
---
+++ src/dbsource/DbProductImpl.cc
@@ -0,0 +1,96 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file zmd/backend/dbsource/DbProductImpl.h
+ *
+*/
+
+#include "DbProductImpl.h"
+#include "zypp/source/SourceImpl.h"
+#include "zypp/TranslatedText.h"
+#include "zypp/base/String.h"
+#include "zypp/base/Logger.h"
+
+using namespace std;
+using namespace zypp::detail;
+
+///////////////////////////////////////////////////////////////////
+namespace zypp
+{ /////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+//
+//        CLASS NAME : DbProductImpl
+//
+///////////////////////////////////////////////////////////////////
+
+/** Default ctor
+*/
+DbProductImpl::DbProductImpl (Source_Ref source_r)
+    : _source (source_r)
+    , _zmdid(0)
+{
+}
+
+/**
+ * read package specific data from handle
+ * throw() on error
+ */
+
+void
+DbProductImpl::readHandle( sqlite_int64 id, sqlite3_stmt *handle )
+{
+    _zmdid = id;
+
+    // 1-5: nvra, see DbSourceImpl
+    const char * text;
+    text = (const char *) sqlite3_column_text( handle, 12 );
+    if (text != NULL)
+	_summary = TranslatedText( string( text ) );
+    text = (const char *) sqlite3_column_text( handle, 13 );
+    if (text != NULL)
+	_description = TranslatedText( string( text ) );
+    return;
+}
+
+
+Source_Ref
+DbProductImpl::source() const
+{ return _source; }
+
+ZmdId DbProductImpl::zmdid() const
+{ return _zmdid; }
+
+/** Product summary */
+TranslatedText DbProductImpl::summary() const
+{ return _summary; }
+
+/** Product description */
+TranslatedText DbProductImpl::description() const
+{ return _description; }
+
+/** Get the category of the product - addon or base*/
+std::string DbProductImpl::category() const
+{ return _category; }
+
+/** Get the vendor of the product */
+Label DbProductImpl::vendor() const
+{ return _vendor; }
+
+/** Get the name of the product to be presented to user */
+Label DbProductImpl::displayName( const Locale & locale_r ) const
+{ return _displayName; }
+
+/** */
+Url DbProductImpl::releaseNotesUrl() const
+{ return _releaseNotesUrl; }
+
+
+  /////////////////////////////////////////////////////////////////
+} // namespace zypp
+///////////////////////////////////////////////////////////////////
