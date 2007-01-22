f615d915dacfbe4dfccb78dfbab4ccb4f94c8505
Klaus Kaempf <kkaempf@suse.de>
2007-01-22 11:53:42 +0000

    Bug 236686: pass callback functor as reference

--- src/locks.cc
+++ src/locks.cc
@@ -1,4 +1,9 @@
-
+//
+// locks.cc
+//
+// read 'locks' table and assign state 'locked:by_user' to pool items
+//
+//
 #include <sqlite3.h>
 #include <set>
 
@@ -22,23 +27,30 @@ using namespace std;
 using namespace boost;
 using namespace zypp;
 
+
+//
+// collect matching names
+//
+// called by regexp matching, see 'Match' below
+//
+
 struct NameMatchCollectorFunc
 {
   set<string> matches;
 
   bool operator()( const PoolItem &item )
   {
-    MIL << "Pool item match number " << matches.size() << ": " << item << endl;
     matches.insert( item.resolvable()->name() );
     return true;
   }
 };
 
+
 // taken from zypper
 struct Match
 {
   const boost::regex * _regex;
-  
+
   Match(const boost::regex & regex ) :
     _regex(&regex)
   {}
@@ -51,7 +63,9 @@ struct Match
   }
 };
 
-string wildcards2regex(const string & str)
+
+string
+wildcards2regex(const string & str)
 {
   string regexed;
 
@@ -71,12 +85,17 @@ string wildcards2regex(const string & str)
   return regexed;
 }
 
+
+//
+// assign Lock to installed pool item
+//
+
 struct ItemLockerFunc
 {
   ItemLockerFunc( const string lock_str )
     : _lock_str(lock_str)
   {}
-  
+
   bool operator()( const CapAndItem &cai_r )
   {
     PoolItem_Ref item(cai_r.item);
@@ -87,9 +106,14 @@ struct ItemLockerFunc
     }
     return true;
   }
-  
+
   string _lock_str;
 };
+
+
+//
+// read 'locks' table, evaluate 'glob' column, assign locks to pool
+//
 
 int
 read_locks (const ResPool & pool, sqlite3 *db)
@@ -110,9 +134,9 @@ read_locks (const ResPool & pool, sqlite3 *db)
     int count = 0;
 
     CapFactory cap_factory;
-    
+
     while ((rc = sqlite3_step (handle)) == SQLITE_ROW)
-    { 
+    {
       // we dont need this data yet
       //string name_str = reinterpret_cast<const char*>(sqlite3_column_text( handle, 1));
       //string version_str = reinterpret_cast<const char*>(sqlite3_column_text( handle, 2));
@@ -120,16 +144,16 @@ read_locks (const ResPool & pool, sqlite3 *db)
       //int epoch = sqlite3_column_int( handle, 4);
       //int arch_id = sqlite3_column_int( handle, 5);
       //RCResolvableRelation rc_rel = (RCResolvableRelation) sqlite3_column_int( handle, 6);
-      
+
       const char *catalog = reinterpret_cast<const char*>(sqlite3_column_text( handle, 7));
       string catalog_str;
       if (catalog)
          catalog_str = catalog;
-      
+
       string glob_str = reinterpret_cast<const char*>(sqlite3_column_text( handle, 8));
       //int importance = sqlite3_column_int( handle, 9);
       //int  importance_gteq = sqlite3_column_int( handle, 10);
-            
+
       // zypp does not provide wildcard or regex support in the Capability matching helpers
       // but it still parses the index if it contains wildcards.
       // so we decompose the capability, and keep the op and edition, while, the name
@@ -137,11 +161,11 @@ read_locks (const ResPool & pool, sqlite3 *db)
       // Then these names are combined with the original edition and relation and we
       // got a new capability for matching wildcard to use with the capability match
       // helpers
-      
+
       Rel rel;
       Edition edition;
       string name;
-      
+
       try
       {
         Capability capability = cap_factory.parse( ResTraits<zypp::Package>::kind, glob_str );
@@ -154,20 +178,23 @@ read_locks (const ResPool & pool, sqlite3 *db)
         ERR << "Can't parse capability in: " << glob_str << " (" << e.msg() << ") skipping" << std::endl;
         continue;
       }
-        
+
+      // Operator NONE is not allowed in Capability
+      if (rel == Rel::NONE) rel = Rel::ANY;
+
       NameMatchCollectorFunc nameMatchFunc;
-      
+
       // regex flags
       unsigned int flags = boost::regex::normal;
       flags |= boost::regex_constants::icase;
       boost::regex reg;
-      
+
       // create regex object
-      string regstr(wildcards2regex(name));
+      string regstr( wildcards2regex( name ) );
       MIL << "regstr '" << regstr << "'" << endl;
       try
       {
-        reg.assign(regstr, flags);
+        reg.assign( regstr, flags );
       }
       catch (regex_error & e)
       {
@@ -177,35 +204,36 @@ read_locks (const ResPool & pool, sqlite3 *db)
         continue;
       }
 
-      invokeOnEach( pool.begin(), pool.end(), Match(reg), nameMatchFunc );
+      invokeOnEach( pool.begin(), pool.end(), Match(reg), functor::functorRef<bool, const PoolItem &>(nameMatchFunc) );
 
       MIL << "Found " << nameMatchFunc.matches.size() << " matches." << endl;
+
       // now we have all the names matching
-      
+
       // for each name matching try to match a capability
-      
-      ItemLockerFunc lockItemFunc(glob_str);
-      
+
+      ItemLockerFunc lockItemFunc( glob_str );
+
       for ( set<string>::const_iterator it = nameMatchFunc.matches.begin(); it != nameMatchFunc.matches.end(); ++it )
       {
         string matched_name = *it;
- 
+
         try
         {
           Capability capability = cap_factory.parse( ResTraits<zypp::Package>::kind, matched_name, rel, edition );
 	  MIL << "Locking capability " << capability << endl;
-          forEachMatchIn( pool, Dep::PROVIDES, capability, lockItemFunc );
+          forEachMatchIn( pool, Dep::PROVIDES, capability, functor::functorRef<bool, const CapAndItem &>(lockItemFunc) );
         }
         catch ( const Exception &e )
         {
           ERR << "Invalid lock: " << e.msg() << std::endl;
         }
         ++count;
-        
+
       }
     }
 
-    sqlite3_finalize (handle);
+    sqlite3_finalize( handle );
 
     if (rc != SQLITE_DONE)
     {
